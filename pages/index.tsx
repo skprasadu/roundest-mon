4c9fd98349becbd53e9f8d614b9ffe6d816525c4
Krishna Prasad <praskrishna@gmail.com>
2022-07-12 17:39:10 -0700

    second commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div className="text-2xl text-red-200">Hello World</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center relative">
+      <div className="text-2xl text-center">Which Pokemon is Rounder</div>
+      <div className="p-2"></div>
+      <div className="border rounded p-8 flex justify-between item-center max-w-2xl">
+        <div className="w-16 h-16 bg-red-200 " />
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-200" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
