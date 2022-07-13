b46d1666c268b810c7231c9d7bb2d50c0e836f84
Krishna Prasad <praskrishna@gmail.com>
2022-07-12 22:44:31 -0700

    added trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,17 @@
-import { trpc } from '@/utils/trpc';
+import { getOptionsForVote } from '@/utils/getRandomPokemon';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  const { data, isLoading } = trpc.useQuery(['hello', { text: 'Krishna' }]);
-
-  if (isLoading) return <div>Loading...</div>;
-
-  if (data) return <div>{data.greeting}</div>;
+  const [first, second] = getOptionsForVote();
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center relative">
       <div className="text-2xl text-center">Which Pokemon is Rounder</div>
       <div className="p-2"></div>
       <div className="border rounded p-8 flex justify-between item-center max-w-2xl">
-        <div className="w-16 h-16 bg-red-200 " />
+        <div className="w-16 h-16 bg-red-800 ">{first}</div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-200" />
+        <div className="w-16 h-16 bg-red-800">{second}</div>
       </div>
     </div>
   );
