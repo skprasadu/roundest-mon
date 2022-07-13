52536d432ac03aa99fad02b1cf0a302bb603691c
Krishna Prasad <praskrishna@gmail.com>
2022-07-13 12:28:00 -0700

    fixed trpc url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import { trpc } from '@/utils/trpc';
 import type { NextPage } from 'next';
 import React, { useState } from 'react';
 
+const buttonClass =
+  'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded';
+
 const Home: NextPage = () => {
   const [ids, updateIds] = useState(() => getOptionsForVote());
   const [first, second] = ids;
@@ -11,6 +14,12 @@ const Home: NextPage = () => {
   const secondPokemon = trpc.useQuery(['get-pokemon-by-id', { id: second }]);
 
   if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
+
+  const voteForRoundest = (selected: number) => {
+    //todo: fire mutation to persist changes
+
+    updateIds(getOptionsForVote());
+  };
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center relative">
@@ -25,6 +34,12 @@ const Home: NextPage = () => {
           <div className="text-xl text-center capitalize mt-[-2rem]">
             {firstPokemon.data?.name}
           </div>
+          <button
+            className={buttonClass}
+            onClick={() => voteForRoundest(first)}
+          >
+            Rounder
+          </button>
         </div>
         <div className="p-8">Vs</div>
         <div className="w-64 h-64 flex flex-col">
@@ -35,6 +50,12 @@ const Home: NextPage = () => {
           <div className="text-xl text-center capitalize mt-[-2rem]">
             {secondPokemon.data?.name}
           </div>
+          <button
+            className={buttonClass}
+            onClick={() => voteForRoundest(second)}
+          >
+            Rounder
+          </button>
         </div>
         <div className="p-2" />
       </div>
